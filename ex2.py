6ccdb77e42d489ef6e66f7cb7568ed7947177e5d
samharshe <samharshe29@gmail.com>
2023-02-18 16:49:26 -0500

    groundbreaking stuff

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# Comments don't run.
+
+print("I could have code like this.") # and the comment after is ignored.
+
+# Comments also disable code:
+# print("This won't run.")
+# and it doesn't run!
+
+print("This will run.")
---
+++ ex3.py
@@ -0,0 +1,35 @@
+# Turned stuff to floats to see what would happen.
+
+# Prints a sentence.
+print("I will now count my chickens.")
+
+# Prints the name of the animal, calculates the number of that animal, then prints that number after the name and a space.
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+# Prints a sentence.
+print("Now I will count the eggs:")
+
+# Does some math, converting 1/4 to 0.25 (which I did not think it would do), and prints the answer.
+print(float(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6))
+
+# Prints a question.
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Calculates the answer to that question and prints it.
+print(float(3 + 2 < 5 - 7))
+
+# Prints a question, calculates the answer to that question, and prints that answer after a space.
+print("What is 3 + 2?", float(3 + 2))
+print("What is 5 - 7?", float(5 - 7))
+
+# Prints a sentence.
+print("Oh, that's why it's False.")
+
+# Prints a sentence.
+print("How about some more.")
+
+# Prints a question, calculates the answer to that question, and prints that answer after a space.
+print("Is it greater?", float(5) > -2)
+print("Is it greater or equal", 5 >= float(-2))
+print("Is it less or equal?", float(5 <= -2))
